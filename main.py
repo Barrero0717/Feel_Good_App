9683db7896374cd921f09139901027e6e5aaf7f9
Andres Felipe Barrero <felipe.barrero17@gmail.com>
2022-01-20 09:24:43 -0500

    Adding code

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+import imp
 from time import strftime
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.animation import Animation
+from hoverable import HoverBehavior
+from kivy.uix.image import Image
+from kivy.uix.behaviors import ButtonBehavior
 import json
+import requests
 from datetime import datetime
 
-
 Builder.load_file("desing.kv")
+
+url = "https://www.affirmations.dev/"
 
 class LoginScreen(Screen):
     def sign_up(self):
@@ -16,11 +23,13 @@ class LoginScreen(Screen):
         with open("users.json") as file:
             users = json.load(file)
         if uname in users and users[uname]['password'] == pword:
-            self.manager.transition.direction = "right"
+            self.manager.transition.direction = "left"
             self.manager.current = "login_screen_success"
         else:
             self.ids.login_wrong.text = "Wrong user or password!"
-            
+    
+    def forgot_password(self):
+        self.manager.current = "under_construction"        
                          
 class RootWidget(ScreenManager):
     pass
@@ -47,6 +56,17 @@ class LoginScreenSuccess(Screen):
     def log_out(self):
         self.manager.transition.direction = "right"
         self.manager.current = "login_screen"
+        
+    def get_quote(self):
+        request = requests.get(url)
+        response = json.loads(request.content)
+        self.ids.quote.text = response["affirmation"]
+
+class ImageButton(ButtonBehavior, HoverBehavior, Image):
+    pass
+
+class UnderConstruction(Screen):
+    pass
 
 class MainApp(App):
     def build(self):
